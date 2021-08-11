dc37178d44f8347303082f6d94c2ba537167addb
kellymgerstner <81729298+kellymgerstner@users.noreply.github.com>
2021-08-10 19:02:02 -0500

    Equals() and HashCode() override added

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -30,5 +30,17 @@ JobCoreCompetency = jobCoreCompetency;
 
 
         // TODO: Generate Equals() and GetHashCode() methods.
+
+public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
     }
 }
