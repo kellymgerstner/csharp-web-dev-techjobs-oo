105a497deae85c70980393bfcd6e485107a64ff4
[Kelly Gerstner] <[kellymgerstner@gmail.com]>
2021-08-11 19:25:32 -0500

    started on Job Constructor test

--- TechJobsTest/JobTests.cs
+++ TechJobsTest/JobTests.cs
@@ -26,7 +26,18 @@ namespace TechJobsTest
         [TestMethod]
         public void TestJobConstructorSetsAllFields()
         {
-            TechJobsOO.Job test_job = new TechJobsOO.Job("Product Tester", "ACME", "Desert", "QualityControl", "Persistence");
+            TechJobsOO.Employer test_employer= new TechJobsOO.Employer("ACME");
+            TechJobsOO.Location test_location = new TechJobsOO.Location("Desert");
+            TechJobsOO.PositionType test_positionType = new TechJobsOO.PositionType("Quality Control");
+            TechJobsOO.CoreCompetency test_coreCompetency = new TechJobsOO.CoreCompetency("Persistence");
+            
+            TechJobsOO.Job test_job = new TechJobsOO.Job("Product Tester", test_employer, test_location, test_positionType, test_coreCompetency);
+            
+            Assert.AreEqual("Product Tester", test_job.Name);
+            //Assert.AreEqual("ACME", test_job.EmployerName);
+            //Assert.AreEqual("Desert", test_job.EmployerLocation);
+            //Assert.AreEqual("QualityControl", test_job.JobType);
+            //Assert.AreEqual("Persistence", test_job.JobCoreCompetency);
 
         }
 
